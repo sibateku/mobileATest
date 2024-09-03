6b7e851b52d5dcb5a10508cc6f7bc6e0885e3f9d
Akito Sakoh <146631166+sibateku@users.noreply.github.com>
2024-09-03 12:21:04 +0900

    ２日目の２限目

--- app/src/main/java/jp/ac/meijou/android/s231205090/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205090/MainActivity.java
@@ -1,24 +1,40 @@
 package jp.ac.meijou.android.s231205090;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s231205090.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.text.setText(R.string.hello);
+
+        var plane = ContextCompat.getDrawable(this, R.drawable.plane);
+        binding.imageView2.setImageDrawable(plane);
     }
 }
