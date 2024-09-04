36fa9c0cca53400b42b2087bd85f39882aa72ad7
Akito Sakoh <146631166+sibateku@users.noreply.github.com>
2024-09-04 16:10:50 +0900

    ３日目終了 複数のページ間でデータをやり取りする動作を作成

--- app/src/main/java/jp/ac/meijou/android/s231205090/MainActivity4.java
+++ app/src/main/java/jp/ac/meijou/android/s231205090/MainActivity4.java
@@ -5,10 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s231205090.databinding.ActivityMain4Binding;
 import jp.ac.meijou.android.s231205090.databinding.ActivityMainBinding;
@@ -16,6 +20,25 @@ import jp.ac.meijou.android.s231205090.databinding.ActivityMainBinding;
 public class MainActivity4 extends AppCompatActivity {
 
     private ActivityMain4Binding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data ->data.getStringExtra("ret"))
+                                .map(ret -> "Result: " + ret)
+                                .ifPresent(text -> binding.view.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.view.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.view.setText("Result: Unknown(" );
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +64,19 @@ public class MainActivity4 extends AppCompatActivity {
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.yahoo.co.jp"));
             startActivity(intent);
+        });
+
+        // 送信ボタン
+        binding.sendButton.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            intent.putExtra("name", binding.text.getText().toString());
+            startActivity(intent);
+        });
+
+        // 起動
+        binding.startButton.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            getActivityResult.launch(intent);
         });
     }
 }
